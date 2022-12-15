b09b247ba1ed0d9afe75352ae4964c66194f2507
chamisupu <chami2170@gmail.com>
2022-12-15 12:45:39 +0530

    Object / Array

---
+++ JS Part 1/index.js
@@ -0,0 +1,9 @@
+let student = { studentName: "kamal", index: "001"};    //object
+
+let teacher = [{ studentName: "kamal", index: "001"}, { studentName: "Nimal", index: "002"}];    //array
+
+console.log(student.studentName);
+console.log(student.index);
+
+console.log(teacher[0].studentName);
+console.log(teacher[1].index);
