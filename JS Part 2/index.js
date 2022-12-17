407692ad9d480c7ad44cdc3a3e331bbfc8ee4ba8
chamisupu <chami2170@gmail.com>
2022-12-17 11:26:07 +0530

    Conditional Statement

---
+++ JS Part 2/index.js
@@ -0,0 +1,13 @@
+let marks = 12;
+
+if (marks > 75){
+    console.log("A");
+} else if (marks > 65){
+    console.log("B");
+} else if (marks > 55){
+    console.log("C");
+} else if (marks > 35){
+    console.log("S");
+} else {
+    console.log("F");
+}
